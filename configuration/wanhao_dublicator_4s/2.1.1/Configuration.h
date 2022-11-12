ce324ea7871a1e82c6f2cefd91aed1581173c9d2
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2022-11-12 16:58:45 +0100

    up

--- configuration/wanhao_dublicator_4s/2.1.1/Configuration.h
+++ configuration/wanhao_dublicator_4s/2.1.1/Configuration.h
@@ -20,8 +20,8 @@
  *
  */
 #pragma once
-
-// #define CONFIG_EXAMPLES_DIR "Wanhao/Duplicator 4S"
+#error "Don't build with import-2.1.x configurations!"
+#error "Use the 'bugfix...' or 'release...' configurations matching your Marlin version."
 
 /**
  * Configuration.h
