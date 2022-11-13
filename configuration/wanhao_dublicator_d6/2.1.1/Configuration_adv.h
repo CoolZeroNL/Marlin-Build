4881f96e18ced50802093daf69d50e6cdf3954a6
CoolZeroNL <info@local-events.nl>
2022-11-13 13:47:49 +0100

    add

--- configuration/wanhao_dublicator_d6/2.1.1/Configuration_adv.h
+++ configuration/wanhao_dublicator_d6/2.1.1/Configuration_adv.h
@@ -3886,22 +3886,23 @@
 #define CUSTOM_MENU_MAIN
 #if ENABLED(CUSTOM_MENU_MAIN)
   // //#define CUSTOM_MENU_MAIN_TITLE "Custom Commands"
-  // #define CUSTOM_MENU_MAIN_SCRIPT_DONE "M117 User Script Done"
-  // #define CUSTOM_MENU_MAIN_SCRIPT_AUDIBLE_FEEDBACK
-  // //#define CUSTOM_MENU_MAIN_SCRIPT_RETURN   // Return to status screen after a script
+  #define CUSTOM_MENU_MAIN_TITLE "Quick Menu"
+  #define CUSTOM_MENU_MAIN_SCRIPT_DONE "M117 User Script Done"
+  #define CUSTOM_MENU_MAIN_SCRIPT_AUDIBLE_FEEDBACK
+  // #define CUSTOM_MENU_MAIN_SCRIPT_RETURN   // Return to status screen after a script
   // #define CUSTOM_MENU_MAIN_ONLY_IDLE         // Only show custom menu when the machine is idle
 
   // #define MAIN_MENU_ITEM_1_DESC "Home & UBL Info"
   // #define MAIN_MENU_ITEM_1_GCODE "G28\nG29 W"
   // //#define MAIN_MENU_ITEM_1_CONFIRM          // Show a confirmation dialog before this action
 
-  // #define MAIN_MENU_ITEM_2_DESC "Preheat for " PREHEAT_1_LABEL
-  // #define MAIN_MENU_ITEM_2_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
-  // //#define MAIN_MENU_ITEM_2_CONFIRM
+  #define MAIN_MENU_ITEM_2_DESC "Preheat for " PREHEAT_1_LABEL
+  #define MAIN_MENU_ITEM_2_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
+  #define MAIN_MENU_ITEM_2_CONFIRM
 
-  // //#define MAIN_MENU_ITEM_3_DESC "Preheat for " PREHEAT_2_LABEL
-  // //#define MAIN_MENU_ITEM_3_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
-  // //#define MAIN_MENU_ITEM_3_CONFIRM
+  #define MAIN_MENU_ITEM_3_DESC "Preheat for " PREHEAT_2_LABEL
+  #define MAIN_MENU_ITEM_3_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
+  #define MAIN_MENU_ITEM_3_CONFIRM
 
   // //#define MAIN_MENU_ITEM_4_DESC "Heat Bed/Home/Level"
   // //#define MAIN_MENU_ITEM_4_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nG28\nG29"
@@ -3911,21 +3912,8 @@
   // //#define MAIN_MENU_ITEM_5_GCODE "G28\nM503"
   // //#define MAIN_MENU_ITEM_5_CONFIRM
 
-  // #define CUSTOM_MENU_MAIN_TITLE "Quick Menu"
-  #define CUSTOM_MENU_MAIN_SCRIPT_DONE "M117 Command Complete"
-  #define CUSTOM_MENU_MAIN_SCRIPT_AUDIBLE_FEEDBACK
-  //#define CUSTOM_MENU_MAIN_SCRIPT_RETURN  // Return to status screen after a script
-
-  #define CONFIG_MENU_ITEM_1_DESC "Preheat for " PREHEAT_1_LABEL
-  #define CONFIG_MENU_ITEM_1_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
-
-  #define MAIN_MENU_ITEM_2_DESC "Preheat for " PREHEAT_2_LABEL
-  #define MAIN_MENU_ITEM_2_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
-
   #define MAIN_MENU_ITEM_3_DESC "Cooldown"
   #define MAIN_MENU_ITEM_3_GCODE "M140 S0\nM104 S0\nM106 S0"
-
-
 
 #endif
  
@@ -3934,13 +3922,18 @@
 // Custom Menu: Configuration Menu
 // #define CUSTOM_MENU_CONFIG
 #if ENABLED(CUSTOM_MENU_CONFIG)
+  //#define CUSTOM_MENU_CONFIG_TITLE "Custom Commands"
+  #define CUSTOM_MENU_CONFIG_SCRIPT_DONE "M117 Wireless Script Done"
+  #define CUSTOM_MENU_CONFIG_SCRIPT_AUDIBLE_FEEDBACK
+  //#define CUSTOM_MENU_CONFIG_SCRIPT_RETURN  // Return to status screen after a script
+  #define CUSTOM_MENU_CONFIG_ONLY_IDLE        // Only show custom menu when the machine is idle
 
-  // #define CONFIG_MENU_ITEM_1_DESC "Wifi ON"
-  // #define CONFIG_MENU_ITEM_1_GCODE "M118 [ESP110] WIFI-STA pwd=12345678"
+  #define CONFIG_MENU_ITEM_1_DESC "Wifi ON"
+  #define CONFIG_MENU_ITEM_1_GCODE "M118 [ESP110] WIFI-STA pwd=12345678"
   //#define CONFIG_MENU_ITEM_1_CONFIRM        // Show a confirmation dialog before this action
 
-  // #define CONFIG_MENU_ITEM_2_DESC "Bluetooth ON"
-  // #define CONFIG_MENU_ITEM_2_GCODE "M118 [ESP110] BT pwd=12345678"
+  #define CONFIG_MENU_ITEM_2_DESC "Bluetooth ON"
+  #define CONFIG_MENU_ITEM_2_GCODE "M118 [ESP110] BT pwd=12345678"
   //#define CONFIG_MENU_ITEM_2_CONFIRM
 
   //#define CONFIG_MENU_ITEM_3_DESC "Radio OFF"
