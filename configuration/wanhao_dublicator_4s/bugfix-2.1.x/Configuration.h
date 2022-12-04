f65cf07f2a110980c3b2010c0a6de79954b91dbd
CoolZeroNL <info@local-events.nl>
2022-12-04 16:18:24 +0100

    add

--- configuration/wanhao_dublicator_4s/bugfix-2.1.x/Configuration.h
+++ configuration/wanhao_dublicator_4s/bugfix-2.1.x/Configuration.h
@@ -1713,12 +1713,12 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
-// #define Y_MIN_POS Y_BED_SIZE   //?
+// #define Y_MIN_POS 0
+#define Y_MIN_POS Y_BED_SIZE   // because of the movement of the limit switch to the front of the machine
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-// #define Y_MAX_POS 0            //?
-#define Y_MAX_POS Y_BED_SIZE
+#define Y_MAX_POS 0            // because of the movement of the limit switch to the front of the machine
+// #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 150
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
