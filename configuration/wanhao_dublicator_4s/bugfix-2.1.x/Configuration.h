430a7596920d01f352dfed0dc70d901029c6d482
CoolZeroNL <info@local-events.nl>
2022-12-11 15:01:11 +0100

    (2022-12-08) for S4

--- configuration/wanhao_dublicator_4s/bugfix-2.1.x/Configuration.h
+++ configuration/wanhao_dublicator_4s/bugfix-2.1.x/Configuration.h
@@ -1694,7 +1694,9 @@
 // :[-1,1]
 #define X_HOME_DIR 1
 #define Y_HOME_DIR 1
-#define Z_HOME_DIR -1
+#define Z_HOME_DIR 1    
+// #define Z_HOME_DIR -1    // JHG
+
 //#define I_HOME_DIR -1
 //#define J_HOME_DIR -1
 //#define K_HOME_DIR -1
