64a33733c36d08e09ded0b8cac2103f769b72884
CoolZeroNL <info@local-events.nl>
2022-12-16 09:42:14 +0100

    max height to 160 for S4

--- configuration/wanhao_dublicator_4s/bugfix-2.1.x/Configuration.h
+++ configuration/wanhao_dublicator_4s/bugfix-2.1.x/Configuration.h
@@ -1186,11 +1186,12 @@
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
 // #define DEFAULT_MAX_FEEDRATE          { 250, 250, 20, 100 }
-#define DEFAULT_MAX_FEEDRATE          { 250, 250, 20, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 250, 250, 20, 50 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 50 } // ...or, set your own edit limits
+  // #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 50 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 100 } // ...or, set your own edit limits
 #endif
 
 /**
@@ -1200,7 +1201,8 @@
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
 // #define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 5000 }
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 500 }
+// #define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 500 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 1000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
