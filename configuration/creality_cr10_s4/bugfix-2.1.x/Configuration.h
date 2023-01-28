c79f4cf2def34a60bcc2f83727332ceb918cdcc8
CoolZeroNL <info@local-events.nl>
2023-01-28 10:51:38 +0100

    finetune config for cr10-s4

--- configuration/creality_cr10_s4/bugfix-2.1.x/Configuration.h
+++ configuration/creality_cr10_s4/bugfix-2.1.x/Configuration.h
@@ -1159,7 +1159,6 @@
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
 #define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define I_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define J_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define K_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
@@ -1175,6 +1174,8 @@
 #define U_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define V_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define W_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+
+#define Z_MIN_ENDSTOP_INVERTING true       // Set to true to invert the logic of the endstop. // JHG
 #define Z_MIN_PROBE_ENDSTOP_INVERTING true // Set to true to invert the logic of the probe. // JHG NPN-type sensor
 //#define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe. //  PNP-type
 
