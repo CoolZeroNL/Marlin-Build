6d18d084552c8d70c85e2e2e9a790dceb3ece091
CoolZeroNL <info@local-events.nl>
2023-01-29 10:35:30 +0100

    zet probe offzet

--- configuration/creality_cr10_s4/bugfix-2.1.x/Configuration.h
+++ configuration/creality_cr10_s4/bugfix-2.1.x/Configuration.h
@@ -1551,7 +1551,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 31.5, 0, 0 }  // JHG
+#define NOZZLE_TO_PROBE_OFFSET { 31.5, 0, -0.9 }  // JHG
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
