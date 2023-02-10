832e6561be68c4f97c8710217509a6664c736cd6
CoolZeroNL <info@local-events.nl>
2023-02-10 13:57:05 +0100

    change neo stuff

--- configuration/creality_cr10_s4_mks_mini_12864_v3/bugfix-2.1.x/Configuration.h
+++ configuration/creality_cr10_s4_mks_mini_12864_v3/bugfix-2.1.x/Configuration.h
@@ -3407,7 +3407,7 @@
 #define NEOPIXEL_LED  // JHG
 #if ENABLED(NEOPIXEL_LED)
   // #define NEOPIXEL_TYPE          NEO_GRBW // NEO_GRBW, NEO_RGBW, NEO_GRB, NEO_RBG, etc.
-  #define NEOPIXEL_TYPE          NEO_GRBW // NEO_GRBW, NEO_RGBW, NEO_GRB, NEO_RBG, etc.    // JHG
+  #define NEOPIXEL_TYPE             NEO_RGB // NEO_GRBW, NEO_RGBW, NEO_GRB, NEO_RBG, etc.    // JHG
                                           // See https://github.com/adafruit/Adafruit_NeoPixel/blob/master/Adafruit_NeoPixel.h
   //#define NEOPIXEL_PIN                4 // LED driving pin
   //#define NEOPIXEL2_TYPE  NEOPIXEL_TYPE
